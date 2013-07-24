322db6f873bff1434768b81dbafeb55020878e64
Joao Trindade <trindade.joao@gmail.com>
2013-07-24 13:26:49 +0100

    Added costum plugins to oh-my zsh

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 all:
 	ln -f -s `pwd`/zshrc ~/.zshrc
 	git clone "https://github.com/robbyrussell/oh-my-zsh.git"
+	ln -s `pwd`/completion.zsh oh-my-zsh/custom/
+	ln -s `pwd`/movement.zsh oh-my-zsh/custom/
---
+++ movement.zsh
@@ -0,0 +1,20 @@
+bindkey -M viins 'jk' vi-cmd-mode
+
+bindkey '^?' backward-delete-char
+bindkey '^H' backward-delete-char
+
+bindkey -v "^A" beginning-of-line
+bindkey -v "^E" end-of-line
+bindkey -v "^K" kill-line
+bindkey -v "^L" clear-screen
+bindkey -v "^R" history-incremental-search-backward
+bindkey -v "^U" kill-whole-line
+bindkey -v "^V" quoted-insert
+bindkey -v "^W" backward-kill-word
+
+bindkey -v "^[[3~" delete-char
+bindkey -v "^[[5~" backward-word
+bindkey -v "^[[6~" forward-word
+
+bindkey -v "^[[A" history-beginning-search-backward
+bindkey -v "^[[B" history-beginning-search-forward
